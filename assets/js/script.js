8c77db523210f1c3441b976a24c06d698af77666
kevinfAAA <kevin.foley21@hotmail.com>
2021-06-18 05:41:22 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function runGame () {
+
+}
+
+function runGame () {
+
+}
+function checkAnswer () {
+
+}
+function calculateCorrectAnswer () {
+
+}
+
+function IncrementScore () {
+
+}
+
+function IncrementWrongAnswer () {
+
+}
+
+function displayAdditionQuestion () {
+
+}
+
+function displaySubtractQuestion () {
+
+}
+
+function displayMultiplyQuestion () {
+
+}
